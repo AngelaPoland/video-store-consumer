159c2ea5072fca58d8784aee11f1583c4e79ca19
AngelaPoland <angelalpoland@gmail.com>
2018-06-19 17:24:32 -0700

    small change to movie.js

--- src/App.js
+++ src/App.js
@@ -46,9 +46,6 @@ class App extends Component {
             customerName={this.state.selectedCustomer}
           />
         </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>
         <MovieSearch />
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import LibraryList from './LibraryList.js'
+import axios from 'axios';
 // import './LibraryMovie.css';
 
 class Movie extends Component {
 
-  addMovieToLibrary = (event) => {
-      console.log(event.target.id);
-      event.preventDefault();
-      this.props.addMovie(this.props.movie);
-    }
+
+    addMovieToLibrary = () => {
+    const URL = `http://localhost:3000/movies/`
+    axios.post(URL + `?title=${this.props.name}&release_date=${this.props.release_date}&image_url=${this.props.image_url}&overview=${this.props.overview}`)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
 
   render() {
     console.log('Rendering a movie')
@@ -32,7 +38,6 @@ class Movie extends Component {
     name: PropTypes.string.isRequired,
     image_url: PropTypes.string,
     id: PropTypes.number.isRequired,
-    image_url: PropTypes.string,
     release_date: PropTypes.string,
     overview: PropTypes.string,
     addMovie: PropTypes.func.isRequired

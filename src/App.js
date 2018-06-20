c3fe610d09cbdb426359b217a6462556ba91bdc2
AngelaPoland <angelalpoland@gmail.com>
2018-06-20 16:20:34 -0700

    adds react router functionality for static view - looks awesome

--- src/App.js
+++ src/App.js
@@ -45,30 +45,60 @@ appLevelmovie = (aMovie) =>{
 
 
   render() {
+
+    const home = () => {
+      return (<p>Welcome to Selam and Angela's video rental store!</p>);
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p>
-          <Rental
-            customer={this.state.selectedCustomer}
-            movieName={this.state.selectedMovie}
-          />
-        </p>
-        <p>
-        <MovieSearch />
-        <CustList
-          appCustomer={this.appLevelcustomer}
-        />
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p>
+      //     <Rental
+      //       customer={this.state.selectedCustomer}
+      //       movieName={this.state.selectedMovie}
+      //     />
+      //   </p>
+      //   <p>
+      //   <MovieSearch />
+      //   <CustList
+      //     appCustomer={this.appLevelcustomer}
+      //   />
+      //
+      //   <LibraryList
+      //     appMovie={this.appLevelmovie}
+      //   />
+      //   </p>
+      //
+      // </div>
+      <Router>
+      <section>
+      <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/search">Search the Database</Link></li>
+      <li><Link to="/library">Rental Library</Link></li>
+      <li><Link to="/customers">Customers</Link></li>
+      <li><Rental
+        customer={this.state.selectedCustomer}
+        movieName={this.state.selectedMovie}
+      /></li>
+      </ul>
 
-        <LibraryList
-          appMovie={this.appLevelmovie}
-        />
-        </p>
+      <hr/>
 
-      </div>
+      <Route exact path="/" component={home}/>
+      <Route path="/search" component={MovieSearch}/>
+      <Route path="/library"
+      render={(props) => <LibraryList {...props} appMovie={this.appLevelmovie} />}
+      />
+      <Route path="/customers"
+      render={(props) => <CustList {...props} appCustomer={this.appLevelcustomer} />}
+      />
+      </section>
+      </Router>
     );
   }
 }

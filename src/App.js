21dae4c70a48f14f1e59ddd4dc5cc5f18db70073
AngelaPoland <angelalpoland@gmail.com>
2018-06-21 17:25:07 -0700

    barely any css accomplished but we have a logo

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import LibraryList from './components/LibraryList.js'
 import MovieSearch from './components/MovieSearch.js'
 import Rental from './components/Rental.js'
 import MovieRentalButton from './components/MovieRentalButton.js';
+import Home from './components/Home.js';
 import './App.css';
 
 class App extends Component {
@@ -30,74 +31,75 @@ class App extends Component {
       selectedCustomer: aCustomer
     })
     console.log('afasdfasdf')
-      console.log(aCustomer)
+    console.log(aCustomer)
   }
 
 
-appLevelmovie = (aMovie) =>{
-  this.setState({
-    selectedMovie: aMovie
-  })
-}
+  appLevelmovie = (aMovie) =>{
+    this.setState({
+      selectedMovie: aMovie
+    })
+  }
 
-// thanks for the inspiration Dan!
-setStatus = (message, type) => {
-  this.setState({
-    status: { message, type }
-  });
-}
+  // thanks for the inspiration Dan!
+  setStatus = (message, type) => {
+    this.setState({
+      status: { message, type }
+    });
+  }
 
-clearStatus = () => {
-  this.setState({ status: { message: '' }})
-}
+  clearStatus = () => {
+    this.setState({ status: { message: '' }})
+  }
 
 
 
   render() {
 
-    const home = () => {
-      return (
-        <div>
-          <p>Welcome to Selam and Angelas video rental store!</p>
-          <iframe src="https://giphy.com/embed/26DMYEDmjttF46vV6" width="382" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/vintage-3d-26DMYEDmjttF46vV6"></a></p>
-        </div>
-      );
-    };
+    // const home = () => {
+    //   return (
+    //     <div>
+    //       <p>Welcome to Selam and Angelas video rental store!</p>
+    //       <iframe src="https://giphy.com/embed/26DMYEDmjttF46vV6" width="382" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/vintage-3d-26DMYEDmjttF46vV6"></a></p>
+    //     </div>
+    //   );
+    // };
 
     return (
       <Router>
-      <section>
-      <ul className="nav-bar">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/search">Search the Database</Link></li>
-      <li><Link to="/library">Rental Library</Link></li>
-      <li><Link to="/customers">Customers</Link></li>
-      </ul>
-      <hr/>
-        <div>
-        <Rental
-          customer={this.state.selectedCustomer}
-          movie={this.state.selectedMovie}
-          setStatus={this.setStatus}
-        />
-        </div>
-        <div>
-          <StatusBar {...this.state.status} clearStatus={this.clearStatus} />
-        </div>
-      <Route exact path="/" component={home}/>
-      <Route path="/search"
-        render={(props) => <MovieSearch {...props} setStatus={this.setStatus} />}
-        />
-      <Route path="/library"
-      render={(props) => <LibraryList {...props} appMovie={this.appLevelmovie} setStatus={this.setStatus} />}
-      />
+        <section>
+          <ul className="nav-bar">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/search">Search the Database</Link></li>
+            <li><Link to="/library">Rental Library</Link></li>
+            <li><Link to="/customers">Customers</Link></li>
+          </ul>
+          <hr/>
+          <div>
+            <Rental
+              customer={this.state.selectedCustomer}
+              movie={this.state.selectedMovie}
+              setStatus={this.setStatus}
+              />
+          </div>
+          <div>
+            <StatusBar {...this.state.status} clearStatus={this.clearStatus} />
+          </div>
+          <div className="router">
+            <Route exact path="/" component={Home}/>
+            <Route path="/search"
+              render={(props) => <MovieSearch {...props} setStatus={this.setStatus} />}
+              />
+            <Route path="/library"
+              render={(props) => <LibraryList {...props} appMovie={this.appLevelmovie} setStatus={this.setStatus} />}
+              />
 
-      <Route path="/customers"
-      render={(props) => <CustList {...props} appCustomer={this.appLevelcustomer} setStatus={this.setStatus} />}
-      />
-
-
-      </section>
+            <Route path="/customers"
+              render={(props) => <CustList {...props} appCustomer={this.appLevelcustomer} setStatus={this.setStatus} />}
+              />
+          </div>
+          
+        </section>
 
       </Router>
     );
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+
+  render() {
+    console.log('Rendering a movie')
+    return (
+      <div className="home" >
+        <p><img src="https://i.imgur.com/cbKZZmu.png?1" alt="logo" /></p>
+      </div>
+      )
+    }
+  }
+
+
+
+  export default Home;
